ca34214d72ae9ff60fa3b3de78da50ae0d9ceb60
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-07 04:00:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,38 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
+def self.all
     @@all
-  end
+end
 
-  def save
+def save
     self.class.all << self
-  end
+end
+
+def self.create 
+  song = Song.new 
+  song.save 
+  song
+end  
+
+def self.new_by_name(song_name) 
+  song = self.new
+  song.name = song_name 
+  song
+ end
+
+def self.create_by_name(song_name)
+  song = self.new
+  song.name = song_name 
+  song.save
+  song 
+end
+
+def self.find_by_name(song_name)
+  self.all.detect{|s| s.name == song_name}
+end
+
+def self.find_or_create_by_name
+
 
 end
